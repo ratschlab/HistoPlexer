22a4a985a89e768975e59a4d6fe780b9dd42cf94
andanison <sonali@mt00211202.usz.ch>
2025-03-11 20:21:29 +0100

    modified script to aggregate evaluation metrics across experiments

--- bin/compare_exps.py
+++ bin/compare_exps.py
@@ -6,23 +6,31 @@ import pandas as pd
 import statistics
 
 
-base_path = '/raid/sonali/project_mvs/nmi_results/cycleGAN'
+base_path = '/raid/sonali/project_mvs/nmi_results'
 markers_list = ["CD16", "CD20", "CD3", "CD31", "CD8a", "gp100", "HLA-ABC", "HLA-DR", "MelanA", "S100", "SOX10"]
 # save_path = '/raid/sonali/project_mvs/results/final_results/quatitative'
 # os.makedirs(save_path, exist_ok=True)
 
-
-keywords = {'cyclegan-MP': 'tupro_cyclegan_channels-all_seed'}
-# keywords = {'ours-MP': '5GP+ASP_selected_snr_nature', 'pix2pix-MP': 'pix2pix_selected_snr_nature', 'pyramidp2p-MP': 'pyramidp2p_selected_snr_nature',
-            # 'ours-SP': '5GP+ASP_selected_snr_nature_pseudo-multiplex', 'pix2pix-SP': 'pix2pix_selected_snr_nature_pseudo-multiplex', 'pyramidp2p-SP': 'pyramidp2p_selected_snr_nature_pseudo-multiplex'}
-step = '495K'
+keywords = {'ours-FM-MP': 'tupro-patches_ours-FM_channels-all_seed-',
+            'ours-FM-SP': 'tupro-patches_ours-FM_channels-all-pseudoplex_seed',
+            'ours-FM-uni2-MP': 'tupro-patches_ours-FM-uni2_channels-all_seed-', 
+            'ours-FM-virchow2-MP': 'tupro-patches_ours-FM-virchow2_channels-all_seed-',
+            'ours-MP': 'tupro_ours_channels-all_seed-',
+            'ours-SP': 'tupro_ours_channels-all-pseudoplex_seed',
+            'pyramidp2p-MP': 'tupro_pyramidp2p_channels-all_seed-',
+            'pyramidp2p-SP': 'tupro_pyramidp2p_channels-all-pseudoplex_seed',
+            'pix2pix-MP': 'tupro_pix2pix_channels-all_seed-',
+            'pix2pix-SP': 'tupro_pix2pix_channels-all-pseudoplex_seed',
+            'cyclegan-MP': 'tupro_cyclegan_channels-all_seed', 
+            'cyclegan-SP': 'tupro_cyclegan_channels-all-pseudoplex_seed'
+            }
 
 list_csv = []
 
 for exp_name in keywords.keys(): 
     print(exp_name)
     
-    exps = glob.glob(os.path.join(base_path + '/*' + keywords[exp_name] + '*'))
+    exps = glob.glob(os.path.join(base_path + '/*/*' + keywords[exp_name] + '*'))
     print(exps)
     print(len(exps))
  
@@ -32,7 +40,10 @@ for exp_name in keywords.keys():
 
     for exp in exps:
         print(exp.split('/')[-1])
-        df_exp = pd.read_csv(os.path.join(exp, 'test_eval', 'all_metrics_per_sample.csv'))
+        
+        metrics_path = sorted(glob.glob(exp + '/test_eval' + '/*/' + 'all_metrics_per_sample.csv'))[-1]
+        print(metrics_path)
+        df_exp = pd.read_csv(metrics_path)
         df_exp = df_exp[~df_exp['PSNR'].isin([np.inf, -np.inf])]
 
         MSSSIM_mean = df_exp['MSSSIM'].mean()
@@ -49,17 +60,19 @@ for exp_name in keywords.keys():
         print('psnr:', PSNR_mean)
         print('rmse:', RMSE_mean)
         print('\n')
-    msssim_mean = statistics.mean(msssim)
-    msssim_stdev = statistics.stdev(msssim)
-    psnr_mean = statistics.mean(psnr)
-    psnr_stdev = statistics.stdev(psnr)
-    rmse_mean = statistics.mean(rmse)
-    rmse_stdev = statistics.stdev(rmse)
+    # round to 3 decimal places
     
-    print('msssim:', msssim_mean, msssim_stdev)
-    print('psnr:', psnr_mean, psnr_stdev)
-    print('rmse:', rmse_mean, rmse_stdev)
-    list_csv.append([exp_name, msssim_mean, msssim_stdev, psnr_mean, psnr_stdev, rmse_mean, rmse_stdev])
-    # list_csv.append([exp_name, msssim_mean, msssim_stdev, psnr_mean, psnr_stdev])
-    print('\n')
+    if len(msssim) > 1: 
+        msssim_mean = round(statistics.mean(msssim), 3)
+        msssim_stdev = round(statistics.stdev(msssim), 3)
+        psnr_mean = round(statistics.mean(psnr), 3)
+        psnr_stdev = round(statistics.stdev(psnr), 3)
+        rmse_mean = round(statistics.mean(rmse), 3)
+        rmse_stdev = round(statistics.stdev(rmse), 3)
+        
+        print('msssim_:', msssim_mean, msssim_stdev)
+        print('psnr_:', psnr_mean, psnr_stdev)
+        print('rmse_:', rmse_mean, rmse_stdev)
+        list_csv.append([exp_name, msssim_mean, msssim_stdev, psnr_mean, psnr_stdev, rmse_mean, rmse_stdev])
+        print('\n')
 
