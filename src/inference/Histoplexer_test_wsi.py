e1c4527e21af4d012fec435f650714aa99b9e81d
andanison <sonali@mt00211202.usz.ch>
2025-03-27 11:27:57 +0100

    deleted file

--- src/inference/Histoplexer_test_wsi.py
+++
@@ -1,6 +0,0 @@
-
-# prep_process: tissue segmentation, tiling, save tiles temporarily?, resolution of image, what downsample factor, also s.t. ultivue images can be used 
-# run inference on tiles, stitch together for wsi
-    # also save features from model for downstream tasks 
-    # also do s.t can save onl;y specific channels / markers
-# no evaluation, just inference 
